dbbb7708fc6b33f03f4c7e40b89290b893bf63e1
Kyle Johnston <kylerjohnston@gmail.com>
2021-01-09 21:31:57 -0500

    day 2 part 2

--- aoc2020/day2.py
+++ aoc2020/day2.py
@@ -10,29 +10,47 @@ import os
 from .config import input_dir
 
 
-def is_valid(pwd_and_policy):
-    """ Takes a string representing a password and policy. They look like:
+def is_valid(pwd_and_policy, policy='count'):
+    """ Takes a string representing a password and policy, and a policy type
+    which must be 'count' or 'positional'. Part 1 uses the 'count' policy,
+    which counts occurances of a character in the password; Part 2 uses the
+    positional policy, which looks at a character's position in the password.
+
+    They look like:
 
     1-3 a: abcde
 
     Returns True if valid; False if not
     """
+    assert policy in ['count', 'positional'], (
+        "Policy must be 'count' or 'positional'")
+
     parser = re.compile(r'(\d+)-(\d+) ([a-z]): (\w+)')
     match = re.match(parser, pwd_and_policy)
-    mini, maxi, char, pwd = match.groups()
+    p1, p2, char, pwd = match.groups()
 
-    if int(mini) <= len(re.findall(char, pwd)) <= int(maxi):
-        return True
+    p1 = int(p1)
+    p2 = int(p2)
+
+    if policy == 'count':
+        if p1 <= len(re.findall(char, pwd)) <= p2:
+            return True
+
+    elif policy == 'positional':
+        positions = [x for x in [pwd[p1-1], pwd[p2-1]] if x == char]
+        if len(positions) == 1:
+            return True
 
     return False
 
 
-def count_valid(pwd_list):
+def count_valid(pwd_list, policy='count'):
     """ Takes a list of password and policy strings.
 
     Returns a count of the valid passwords.
     """
-    return len([x for x in pwd_list if is_valid(x)])
+    return len([x for x in pwd_list if is_valid(x, policy=policy)])
+
 
 def run():
     """ Day 2 """
@@ -42,3 +60,5 @@ def run():
         input_data = f.readlines()
 
         print(f'Part 1: {count_valid(input_data)}')
+        part2 = count_valid(input_data, policy='positional')
+        print(f"Part 2: {part2}")
--- tests/test_day2.py
+++ tests/test_day2.py
@@ -2,7 +2,7 @@
 
 from aoc2020.day2 import is_valid, count_valid
 
-day1_test_data = [
+day2_test_data = [
     "1-3 a: abcde",
     "1-3 b: cdefg",
     "2-9 c: ccccccccc"]
@@ -11,4 +11,11 @@ def test_day2_part1():
     assert is_valid("1-3 a: abcde")
     assert is_valid("2-9 c: ccccccccc")
     assert not is_valid("1-3 b: cdefg")
-    assert count_valid(day1_test_data) == 2
+    assert count_valid(day2_test_data) == 2
+
+
+def test_day2_part2():
+    assert is_valid("1-3 a: abcde", policy='positional')
+    assert not is_valid("2-9 c: ccccccccc", policy='positional')
+    assert not is_valid("1-3 b: cdefg", policy='positional')
+    assert count_valid(day2_test_data, policy='positional') == 1
